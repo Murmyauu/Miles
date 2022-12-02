328991e0beac09189c1e75405c66d4eb4934adbc
Munira <munira.ablyazova@gmail.com>
2022-12-02 18:09:17 +0500

    Changed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket = 2099;
-        int oneMile = 20;
-        int mile = (ticket / oneMile);   //104
+        int ticketPrice = 2099;
+        int mileCost = 20;
+        int onemile = (ticketPrice / mileCost);   //104
 
-        System.out.println(mile + " Бонусные рубли" );
+        System.out.println(onemile + " Бонусные рубли" );
     }
 }
