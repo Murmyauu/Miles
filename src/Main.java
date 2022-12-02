deb98c84c604e50e71f8e3db7fe8385092039481
Munira <munira.ablyazova@gmail.com>
2022-12-02 17:56:29 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 2099;
+        int oneMile = 20;
+        int mile = (ticket / oneMile);   //104
+
+        System.out.println(mile + " Бонусные рубли" );
+    }
+}
