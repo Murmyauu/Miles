64fc1d3ab49bb1a99fde3fd875a3ad14f6630d26
Munira <munira.ablyazova@gmail.com>
2022-12-05 16:52:04 +0500

    Renaming a variable

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 2099;
-        int miles = service.calculate(cost);
+        int price = 2099;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
